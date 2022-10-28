fa082408c95f22212d053eaa1bd498b0540c6926
= <spencerbacay@gmail.com>
2022-10-29 06:45:15 +0800

    add navbar

---
+++ Web/src/constants/constants.tsx
@@ -0,0 +1,25 @@
+import AboutUs from "pages/AboutUs/AboutUs";
+import Home from "pages/Home/Home";
+import { ReactRoutesType } from "./type";
+import PageWrapper from "../layouts/PageWrapper/PageWrapper";
+
+/* Web App Constant Images */
+export const IMAGES = {
+  COMPANY_LOGOS: {
+    MAIN: "/assets/logos/comfort-life-financial-main-logo.png",
+  },
+};
+
+export const VIDEO_FINANCING = "/assets/video/coming-soon-video.mp4";
+export const COMPANY_NAME = "Comfort Life Financial";
+
+export const REACT_ROUTES: ReactRoutesType[] = [
+  {
+    PATH: "/",
+    ELEMENT: <PageWrapper component={<Home />} />,
+  },
+  {
+    PATH: "/about",
+    ELEMENT: <PageWrapper component={<AboutUs />} />,
+  },
+];
--- Web/src/index.tsx
+++ Web/src/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import ComingSoon from "library/ComingSoon/ComingSoon";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { REACT_ROUTES } from "./constants/constants";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ComingSoon />
+    <BrowserRouter>
+      <Routes>
+        {REACT_ROUTES.map((ROUTE, index) => (
+          <React.Fragment key={index}>
+            <Route path={ROUTE.PATH} element={ROUTE.ELEMENT} />
+          </React.Fragment>
+        ))}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
